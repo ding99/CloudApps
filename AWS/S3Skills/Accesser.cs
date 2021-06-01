66e04695293c6952f139d2f5b83b79e917351f27
Wei Ding <ding_web@yahoo.co.jp>
2021-06-01 17:38:44 -0400

    list files

--- AWS/S3Skills/Accesser.cs
+++ AWS/S3Skills/Accesser.cs
@@ -22,12 +22,16 @@ namespace S3Skills {
 			Console.WriteLine($"There are ({listResponse.Buckets.Count}) bucket{(listResponse.Buckets.Count < 2 ? "" : "s")} totally.");
 
 			try {
+
 				foreach (var a in listResponse.Buckets) {
+					Console.ForegroundColor = ConsoleColor.Yellow;
 					Console.WriteLine($"  Bucket Name: [{a.BucketName}] {"\t"} Creation Date: [{a.CreationDate}]");
 
-					ListObjectsRequest request = new ListObjectsRequest();
-					request.BucketName = a.BucketName;
-					request.Prefix = ".";
+					Console.ForegroundColor = ConsoleColor.Cyan;
+					ListObjectsRequest request = new ListObjectsRequest { BucketName = a.BucketName };
+					ListObjectsResponse response = await s3.ListObjectsAsync(request);
+					foreach(var b in response.S3Objects)
+						Console.WriteLine($"    Key:[{b.Key}], Modified:[{b.LastModified}], Size:[{b.Size}], Storage:[{b.StorageClass}]");
 				}
 			}
 			catch(Exception e) {
